ac615d47aafb8999aeef7b73d0d6303e6d2c36bb
ArshilHapani <arshilhapani998@gmail.com>
2024-11-19 23:10:41 +0530

    wrote  tests

--- snapshots/src/lib.rs
+++ snapshots/src/lib.rs
@@ -1,6 +1,6 @@
-use std::{error::Error, fs::File, io::Read, path::PathBuf};
-
 pub mod state;
+
+use std::{error::Error, fs::File, io::Read, path::PathBuf};
 
 pub fn hash_file(path: &PathBuf) -> Result<String, Box<dyn Error>> {
     let file_result = File::open(path);
@@ -21,3 +21,27 @@ pub fn hash_file(path: &PathBuf) -> Result<String, Box<dyn Error>> {
     }
 }
 // TODO implement further snapshot functions
+
+#[cfg(test)]
+mod hash_file_test {
+    use super::*;
+    use std::time::SystemTime;
+
+    /// Try changing the content of `Cargo.toml` file inside **snapshots** dir and rerun the test!
+    #[test]
+    fn test_hash_file() {
+        let st = SystemTime::now();
+        let file = PathBuf::from("Cargo.toml"); // snapshots/Cargo.toml file
+        let hash = hash_file(&file).unwrap_or("".to_string());
+        println!("{}", hash);
+        assert_eq!(
+            hash,
+            "abc318a7a48ea6169c95461f05ac28f133712f08af64fc2b970b3a1bacd8ff30"
+        );
+        let st_end = SystemTime::now();
+        println!("{:?}", st);
+        println!("{:?}", st_end);
+
+        assert!(st < st_end);
+    }
+}
